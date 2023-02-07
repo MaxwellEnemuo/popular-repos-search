f0d005c8f2cecae4741c69d5f9ee1a67c33ef0a7
maxwellenemuo <maxwell.enemuo@gmail.com>
2023-02-07 10:58:54 +0100

    validation

--- src/app.ts
+++ src/app.ts
@@ -11,12 +11,23 @@ app.use(express.json());
 app.use(
   [
     body("date")
-      .trim()
-      .isString()
+      .not()
+      .isEmpty()
+      .withMessage("Date is required")
+      .bail()
       .matches(/^\d{4}-\d{2}-\d{2}$/)
-      .withMessage("Date must be in YYYY-MM-DD format"),
-    body("language").trim().isString().optional({ nullable: true }),
-    body("per_page").isInt().isIn([10, 50, 100]).optional({ nullable: true }),
+      .withMessage("Date must be in YYYY-MM-DD format."),
+    body("per_page")
+      .isInt()
+      .withMessage("It must be an integer.")
+      .bail()
+      .isIn([10, 50, 100])
+      .withMessage("Limit must be either 10, 50 or 100.")
+      .optional({ nullable: true }),
+    body("language")
+      .isString()
+      .withMessage("Language must be a string.")
+      .optional({ nullable: true }),
   ],
   (req: Request, res: Response, next: NextFunction) => {
     const errors = validationResult(req);
